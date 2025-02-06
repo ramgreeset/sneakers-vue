4e64f42367d2b1fd62ca8f5d9de6fb0b1e48c74b
Roman Artemyev <artemyev.ram@gmail.com>
2025-02-06 19:04:21 +0300

    Простой счетчик на Vue Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
+<!--
+SFC - single file component
+Логика и стили для компонента содержится в самом файле компонента
+-->
+<!--
+setup - указывает что мы работаем с Composition API
+-->
 <script setup>
+import FirstComponent from "@/components/Counter.vue";
 </script>
-
+<!--В template размещается верстка компонента, вся верстка вне template не отображается-->
 <template>
+  <h1>Привет</h1>
+
+  <FirstComponent/>
 </template>
 
+<!--
+scoped - указывает что стили применяются локально к текущему файлу
+-->
 <style scoped>
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<script setup>
+//Прокидываем ref для динамического отображения изменения состояния
+import {ref} from "vue";
+let count = ref(0);
+function counter() {
+  count.value++
+}
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+<!--Запускаем функцию по клику-->
+  <button @click="counter">+</button>
+</template>
+
+<style scoped>
+h1 {
+  color: aqua;
+}
+</style>
