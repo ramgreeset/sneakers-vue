afddb3842ed1f1002344143581b3d703d80ffb3f
Roman Artemyev <artemyev.ram@gmail.com>
2025-02-22 16:05:03 +0300

    Завершение верстки компонента Header

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,11 @@
 <script setup>
 
+import Header from '@/components/Header.vue'
 </script>
 <template>
   <body class="h-screen flex flex-col w-4/5 mx-auto mt-14">
-  <header class="bg-white rounded-t-xl container border-b-2">
-    <div class="flex justify-between py-6">
-
-      <div class="flex">
-        <img class="size-16" src="/logo.png" alt="logo">
-        <div class="flex flex-col justify-center">
-          <h2 class="text-3xl font-semibold">Sneakers-Vue</h2>
-          <p>Магазин кроссовок</p>
-        </div>
-      </div>
-
-      <div class="flex items-center font-semibold">
-        <nav>
-          <ul class="flex gap-4">
-            <li class="flex gap-2">
-              <img src="/cart.svg" alt="Корзина">
-              <p>12300р</p>
-            </li>
-            <li class="flex gap-2">
-              <img src="/heart.svg" alt="Избранное">
-              <p>Избранное</p>
-            </li>
-            <li class="flex gap-2">
-              <img src="/profile.svg" alt="Профиль">
-              <p class="flex">Профиль</p>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
-    </div>
-  </header>
-  <main class="flex-grow bg-white">
+  <Header/>
+  <main class="flex-grow container bg-white">
     <div class="bg-white">
       <h1>sdasd</h1>
     </div>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="bg-white rounded-t-xl container border-b-2">
+    <div class="flex justify-between py-6">
+
+      <div class="flex gap-4">
+        <img class="size-16" src="/logo.png" alt="logo">
+        <div class="flex flex-col justify-center">
+          <h2 class="text-3xl font-semibold">Sneakers-Vue</h2>
+          <p>Магазин кроссовок</p>
+        </div>
+      </div>
+
+      <div class="flex items-center font-semibold">
+        <nav>
+          <ul class="flex gap-4">
+            <li class="flex gap-2 cursor-pointer hover:text-emerald-700">
+              <img src="/cart.svg" alt="Корзина">
+              <p>12300р</p>
+            </li>
+            <li class="flex gap-2 cursor-pointer hover:text-emerald-700">
+              <img src="/heart.svg" alt="Избранное">
+              <p>Избранное</p>
+            </li>
+            <li class="flex gap-2 cursor-pointer hover:text-emerald-700 ">
+              <img src="/profile.svg" alt="Профиль">
+              <p>Профиль</p>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
